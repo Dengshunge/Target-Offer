ee9e9a931fe19a3c6a1967ecf577674ed76eef3f
Deng shunge <dengshunge2013@163.com>
2018-06-24 13:18:25 +0800

    Update test13_机器人的运动范围.py

--- test13_机器人的运动范围.py
+++ test13_机器人的运动范围.py
@@ -6,29 +6,36 @@
     https://www.cnblogs.com/54Leo/p/6133270.html
     https://github.com/Jack-Lee-Hiter/AlgorithmsByPython/blob/master/Target%20Offer/%E6%9C%BA%E5%99%A8%E4%BA%BA%E7%9A%84%E8%BF%90%E5%8A%A8%E8%8C%83%E5%9B%B4.py
 和面试题12一样使用回溯法，进行迭代，套路很相近
+
+另外，用visited = [[False] * rows ]* cols这种方式创建二维数组，是个坑，虽然行，但每当都是array的引用。
+应该用test = [[0 for i in range(m)] for j in range(n)]这种方式。
+https://www.cnblogs.com/PyLearn/archive/2017/11/06/7795552.html
+
 '''
 
 class Solution:
     def movingCount(self,threshold,rows,cols):
+        import numpy as np
         if threshold < 0 or rows<= 0 or cols <= 0:
             return 0
-        visited = [False] * (rows * cols)
+        visited = [[0 for i in range(rows)]for j in range(cols)]
         count = self.movingCountCore(threshold,rows,cols,0,0,visited)
         return count
 
     def movingCountCore(self,threshold,rows,cols,i,j,visited):
         count = 0
         if self.check(threshold,rows,cols,i,j,visited):
-            visited[i * cols + j] = True
+            a = visited[1][0]
+            visited[i][j] = True
             count = 1 + self.movingCountCore(threshold,rows,cols,i-1,j,visited) \
                       + self.movingCountCore(threshold,rows,cols,i+1,j,visited) \
                       + self.movingCountCore(threshold,rows,cols,i,j-1,visited) \
                       + self.movingCountCore(threshold,rows,cols,i,j+1,visited)
         return count
 
-    def check(self, threshold, rows, cols, row, col, visited):
-        if 0<=row<rows and 0<=col<cols  and (self.getDigitSum(row,col)<= threshold) \
-                and not visited[row * cols + col]:
+    def check(self, threshold, rows, cols, i, j, visited):
+        if 0<=i<rows and 0<=j<cols  and self.getDigitSum(i,j)<= threshold \
+                and not visited[i][j]:
             return True
         return False
 
