68f92d0599b2882d91e77e85fe02a2d2b1f656e5
Deng shunge <dengshunge2013@163.com>
2018-07-20 12:45:44 +0800

    Create test38_字符串的排列.py

---
+++ test38_字符串的排列.py
@@ -0,0 +1,23 @@
+# 面试题38 字符串的排列
+'''
+全排列算法，很常见，利用递归
+固定第i个数组，然后处理后面n-i个数组
+这里需要交换2个位置，但最后会交换回来。
+'''
+
+def Permutation(lis):
+    if not isinstance(lis,list):
+        return
+    Permutation_(lis,0)
+
+def Permutation_(lis,n):
+    if n >= len(lis):
+        print(' '.join(lis))
+        return
+    for i in range(n,len(lis)):
+        lis[i], lis[n] = lis[n], lis[i]  # 交换两个位置
+        Permutation_(lis,n+1)# 注意，这里是n+1
+        lis[i], lis[n] = lis[n], lis[i]  # 交换回来
+
+lis = list('abc')
+Permutation(lis)
